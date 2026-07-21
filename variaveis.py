a273e533e2abb4613895392b914d7499a82f7831
MatheusNunes3001 <matheusmnb03@gmail.com>
2026-07-21 18:30:54 -0400

    adicionando Variaveis emPython

---
+++ variaveis.py
@@ -0,0 +1,20 @@
+qtd_em_estoque = 100
+descricao = 'Cabo Multiplex Trifasico'
+preco_unitario = 17.50
+unidade_de_medida = 'metros'
+preco_por_metro = 17.50 
+colors = [ 'vermelho', 'preto', 'cinza' ]
+print (descricao)
+print (preco_unitario)
+print (unidade_de_medida)
+print (preco_por_metro)
+print (colors)
+print (qtd_em_estoque)
+print ('Descrição: ' + descricao)
+print ('Preço Unitário: R$ ' + str(preco_unitario))
+print('Unidade de Medida: ' + unidade_de_medida)
+print ( 'Preço por Metro: R$ '+ str(preco_por_metro) )
+print ( 'Cores Disponíveis: ' + ', '.join(colors) )
+print ('Quantidade em Estoque: ' +str (qtd_em_estoque))
+ git add variaveis.py 
+git commit -m '' Adicionando Variaveis em Python ''
